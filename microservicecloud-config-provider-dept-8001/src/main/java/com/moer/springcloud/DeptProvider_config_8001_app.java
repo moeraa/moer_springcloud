c80ed7e510aa254059bf6da09508a7bfdf9aa3b9
moeraa <994469740@qq.com>
2019-08-18 18:35:55 +0800

    modified

--- microservicecloud-config-provider-dept-8001/src/main/java/com/moer/springcloud/DeptProvider_config_8001_app.java
+++ microservicecloud-config-provider-dept-8001/src/main/java/com/moer/springcloud/DeptProvider_config_8001_app.java
@@ -3,7 +3,7 @@ package com.moer.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.*;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: moer
@@ -13,9 +13,9 @@ import org.springframework.cloud.netflix.eureka.*;
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @EnableDiscoveryClient
-public class DeptProvider8001_app {
+public class DeptProvider_config_8001_app {
 
   public static void main(String[] args) {
-    SpringApplication.run(DeptProvider8001_app.class, args);
+    SpringApplication.run(DeptProvider_config_8001_app.class, args);
   }
 }
